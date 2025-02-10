efccf41c46cf6283d64d95b1e6aa53f1384213ad
Raissa <raissanavarro66@gmail.com>
2025-02-10 11:20:11 -0300

    11:20

--- src/main/java/com/example/login_auth_api/domain/user/User.java
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -1,15 +1,11 @@
 package com.example.login_auth_api.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
--- src/main/java/com/example/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/example/login_auth_api/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.example.login_auth_api.repositories;
+import com.example.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 
 }
